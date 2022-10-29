606bfb38a9f0fdd60097624720c83ea7806d2731
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2022-10-28 21:24:02 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
 for /f "delims=" %%A in (pkgs.txt) do (
 pip download %%A -d "/output" --no-deps --no-binary :all:
+tar xvfz "/output/*.tar.gz"
+del "/output/*.tar.gz"
 pip wheel -w /builds /output/ 
 )
