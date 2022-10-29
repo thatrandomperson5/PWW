a0147edc7f0a691227ddb24c9bac9bd39243ae2a
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2022-10-29 09:41:30 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,15 +1,17 @@
 @echo off
+setlocal enabledelayedexpansion
 
 for /f "delims=" %%A in (pkgs.txt) do (
     echo Downloading %%A
     pip download %%A -d "/output" --no-deps --no-binary :all:
-    for %%f in (/output/*.tar.gz) do (
-        set archive=%%f
-        call :process %%A
-    )
+    >output.tmp dir /b /output/*.tar.gz
+    <output.tmp set /p "pkg="
+    del output.tmp
+    call :process %%A !pkg!
 )
 
 :process
+set "archive=%~2"
 echo Archive located: %archive%
 tar -xvzf "/output/%archive%"
 del "/output/%archive%"
